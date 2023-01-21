0ad8452d5a665c2eacb992b9552c1686a5c47792
akshayd2020 <63755431+akshayd2020@users.noreply.github.com>
2023-01-21 09:57:45 -0500

    Add temp Windows fix

--- backend/src/loaders/mongoose.ts
+++ backend/src/loaders/mongoose.ts
@@ -3,10 +3,13 @@ import { Db } from 'mongodb';
 import config from '@/config';
 
 export default async (): Promise<Db> => {
-  const connection = await mongoose.connect(config.databaseURL, {
+  const connection = await mongoose.connect('mongodb://localhost:27017/admin', {
+    user: encodeURIComponent('root'),
+    pass: encodeURIComponent('password'),
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
   });
   return connection.connection.db;
 };
+// config.databaseURL
