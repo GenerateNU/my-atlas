24a74126bddaa55cbe2dddfdfa62f4aa102ce230
Ivan Rosales <ivan.gerard@crearth.com>
2023-02-07 14:24:05 -0500

    Added typescript files

---
+++ frontend/App.tsx
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import AppleHealthKit, {
+  HealthValue,
+  HealthKitPermissions,
+} from 'react-native-health'
+
+/* Permission options */
+const permissions = {
+  permissions: {
+    read: [AppleHealthKit.Constants.Permissions.HeartRate],
+    write: [AppleHealthKit.Constants.Permissions.Steps],
+  },
+} as HealthKitPermissions
+
+AppleHealthKit.initHealthKit(permissions, (error: string) => {
+  /* Called after we receive a response from the system */
+
+  if (error) {
+    console.log('[ERROR] Cannot grant permissions!')
+  }
+
+  /* Can now read or write to HealthKit */
+
+  const options = {
+    startDate: new Date(2020, 1, 1).toISOString(),
+  }
+
+  AppleHealthKit.getHeartRateSamples(
+    options,
+    (callbackError: string, results: HealthValue[]) => {
+      /* Samples are now collected from HealthKit */
+    },
+  )
+})
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi, this is My Atlas.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
