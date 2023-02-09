1fe75210a4a5f3ce9da126b5a07fe74c6f774113
Dave Budhram <davebudhram18@gmail.com>
2023-02-09 13:54:57 -0500

    adding functionality for getting, updating, and deleting onboarding

--- backend/src/services/onboarding.ts
+++ backend/src/services/onboarding.ts
@@ -14,10 +14,48 @@ export default class OnboardingService {
 
   public async addOnboarding(onboardingInputDTO: IOnboardingInputDTO): Promise<{ onboarding: IOnboarding }> {
     try {
+      this.logger.debug(onboardingInputDTO);
       const onboardingRecord = await this.onboardingModel.create({
         ...onboardingInputDTO,
       });
-      const onboarding = onboardingRecord.toObject();
+      this.logger.debug("Here");
+      const onboarding :IOnboarding= onboardingRecord.toObject();
+      return { onboarding };
+    } catch (e) {
+      this.logger.error(e);
+      throw e;
+    }
+  }
+  // Gets the onboarding information associated with the given userID (not the 
+  // objectID)
+  public async getOnboarding(userID: string): Promise<{ onboarding: IOnboarding }> {
+    try {
+      const onboardingRecord = await this.onboardingModel.findOne({userID: userID});
+      const onboarding : IOnboarding = onboardingRecord.toObject();
+      return { onboarding };
+    } catch (e) {
+      this.logger.error(e);
+      throw e;
+    }
+  }
+  // Deletes the onboarding information associated with the given userID (not the 
+  // objectID). Returns a message if successfully deleted onboarding information from the database
+  public async deleteOnboardingByUserID(userID: string): Promise<{ message: string }> {
+    try {
+      await this.onboardingModel.deleteOne({userID: userID});
+      const message: string = "Successfully deleed onboarding inforomation associated with user " + userID; 
+      return { message };
+    } catch (e) {
+      this.logger.error(e);
+      throw e;
+    }
+  }
+  public async updateOnboardingByUserID(onboardingInputDTO: IOnboardingInputDTO): Promise<{ onboarding: IOnboarding  }> {
+    try {
+      const userID = onboardingInputDTO.userID;
+      const onboardingRecord = await this.onboardingModel.findOneAndUpdate({userID: userID}, onboardingInputDTO,{
+        new: true}); // new implies we want to return the new document
+      const onboarding : IOnboarding = onboardingRecord.toObject();
       return { onboarding };
     } catch (e) {
       this.logger.error(e);
