c723833f15714dc8a287395c93f85d529afadfd0
matherg <gbomather@gmail.com>
2023-02-08 20:17:32 -0500

    remove user2

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import auth from './routes/auth';
 import user from './routes/user';
-import user2 from './routes/user2';
 import agendash from './routes/agendash';
 import onboarding from './routes/onboarding';
 // guaranteed to get dependencies
@@ -10,7 +9,6 @@ export default () => {
   auth(app);
   user(app);
   agendash(app);
-  user2(app);
   onboarding(app);
   return app;
 };
