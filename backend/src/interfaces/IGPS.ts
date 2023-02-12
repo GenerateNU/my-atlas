355bdee4034cdf6c54d6284f97afa5c1abf2c985
Dave Budhram <davebudhram18@gmail.com>
2023-02-12 16:26:16 -0500

    adding IGPS

--- backend/src/interfaces/IGPS.ts
+++ backend/src/interfaces/IGPS.ts
@@ -1,10 +1,10 @@
-// Inteface for IGPS, 
+// Inteface for IGPS,
 // latatitude, longiitude, altitude, accuracy, and timestamp are optional incase they can't be retreived
 export interface IGPS {
     userID: String; // meta data for GPS, will use userID
     latitude?: number; // not sure if this should be a number or not, in expo it returns a number
     longitude?: number;
-    altitude?: number; 
+    altitude?: number;
     accuracy?: number;
     timestamp: number;
 }
@@ -13,7 +13,7 @@ export interface IGPSInputDTO {
     metadata: {userID: String};
     latitude?: number; // not sure if this should be a number or not, in expo it returns a number
     longitude?: number;
-    altitude?: number; 
+    altitude?: number;
     accuracy?: number;
     timestamp?: number;
 }
