3eac42f40f71dbe8bc491c396eec52d423145248
Michelle Freeman <freeman.mi@northeastern.edu>
2023-02-15 20:55:19 -0500

    Michelle commit after cloning

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,5 +1,6 @@
 import { Document, Model } from 'mongoose';
 import { IUser } from '@/interfaces/IUser';
+import { IActivity} from '../../interfaces/IActivity'
 declare global {
   namespace Express {
     export interface Request {
@@ -9,5 +10,6 @@ declare global {
 
   namespace Models {
     export type UserModel = Model<IUser & Document>;
+    export type ActivityModel = Model<IActivity & Document>
   }
 }
