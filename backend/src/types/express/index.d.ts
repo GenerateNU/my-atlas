b8bab8f78ee6f024e39032c017443688d437f000
Dave Budhram <davebudhram18@gmail.com>
2023-02-15 20:12:47 -0500

    adding global gps model

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -2,7 +2,7 @@ import { Document, Model } from 'mongoose';
 import { IUser } from '@/interfaces/IUser';
 import {IOnboarding} from "@/interfaces/IOnboarding";
 import { IActivity} from '../../interfaces/IActivity'
-
+import { IGPS } from '../../interfaces/IGPS';
 declare global {
   namespace Express {
     export interface Request {
@@ -14,6 +14,6 @@ declare global {
     export type UserModel = Model<IUser & Document>;
     export type OnboardingModel = Model<IOnboarding & Document>;
     export type ActivityModel = Model<IActivity & Document>
-
+    export type GPSModel = Model<IGPS & Document>;
   }
 }
