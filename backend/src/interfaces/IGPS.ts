5d849b66f33b8e76205b97d465485022a95d6e0f
matherg <gbomather@gmail.com>
2023-02-22 18:18:23 -0500

    Rebase from Main

--- backend/src/interfaces/IGPS.ts
+++ backend/src/interfaces/IGPS.ts
@@ -1,19 +1,19 @@
 // Inteface for IGPS,
 // latatitude, longiitude, altitude, accuracy, and timestamp are optional incase they can't be retreived
 export interface IGPS {
-    userID: String; // meta data for GPS, will use userID
-    latitude?: number; // not sure if this should be a number or not, in expo it returns a number
-    longitude?: number;
-    altitude?: number;
-    accuracy?: number;
-    timestamp: number;
+  userID: string; // meta data for GPS, will use userID
+  latitude?: number; // not sure if this should be a number or not, in expo it returns a number
+  longitude?: number;
+  altitude?: number;
+  accuracy?: number;
+  timestamp: number;
 }
 
 export interface IGPSInputDTO {
-    userID: String;
-    latitude?: number; // not sure if this should be a number or not, in expo it returns a number
-    longitude?: number;
-    altitude?: number;
-    accuracy?: number;
-    timestamp: number;
+  userID: string;
+  latitude?: number; // not sure if this should be a number or not, in expo it returns a number
+  longitude?: number;
+  altitude?: number;
+  accuracy?: number;
+  timestamp: number;
 }
