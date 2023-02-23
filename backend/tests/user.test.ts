89ab3af19f87649ebfe569758c335f467c84c721
akshayd2020 <63755431+akshayd2020@users.noreply.github.com>
2023-02-22 20:43:33 -0500

    MA-9 Add testing framework (#10)
    Co-authored-by: Akshay Dupuguntla <akshayd2020@Akshays-MacBook-Pro.local>

---
+++ backend/tests/user.test.ts
@@ -0,0 +1,35 @@
+import { IUserInputDTO } from '../src/interfaces/IUser';
+import AuthService from '../src/services/auth';
+import User from "../src/models/user";
+import LoggerInstance from "../src/loaders/logger";
+import { EventDispatcher as EventDispatcherClass } from 'event-dispatch';
+// Next 4 lines required in every test file
+const db = require('./db');
+
+beforeAll(async () => await db.connect(), 18000);
+let mongoServer: any;
+
+afterEach(async () => await db.clearDatabase());
+
+afterAll (async () => await db.closeDatabase());
+
+const eventDispatcher = new EventDispatcherClass();
+const authServiceInstance = new AuthService(User, LoggerInstance, eventDispatcher);
+
+describe("Add User document to database", () => {
+    it('Add document', async done => {
+        const userExample: IUserInputDTO = {
+            name: "test-user",
+            email: "testemail@test.com",
+            password: "test",
+            phoneNumber: "8888888888",
+            dob: "01/01/2001",
+            age: 20
+        }
+        const {user, token} = await authServiceInstance.SignUp(userExample);
+        const userFromDB = await User.findById(user._id);
+        expect(userFromDB.name).toEqual("test-user");
+        expect(userFromDB.email).toEqual("testemail@test.com");
+        done();
+    })
+})
