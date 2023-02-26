2073779126caec23a1c5455c9ec1cf7ec63c3d73
Ivan Rosales <ivan.gerard@crearth.com>
2023-02-25 19:44:05 -0500

    Changed name of audio exposure models

---
+++ backend/src/interfaces/IEnvironmentalAudioExposure.ts
@@ -0,0 +1,16 @@
+export interface IEnvironmentalAudioExposure {
+  _id: String;
+  userID: String;
+  startDate: Date;
+  duration: Number;
+  value: Number;
+  hkID: String;
+}
+
+export interface IEnvironmentalAudioExposure {
+  userID: String;
+  startDate: Date;
+  duration: Number;
+  value: Number;
+  hkID: String;
+}
---
+++ backend/src/models/environmentalAudioExposure.ts
@@ -0,0 +1,39 @@
+import { IEnvironmentalAudioExposure } from '../interfaces/IEnvironmentalAudioExposure';
+import mongoose from 'mongoose';
+
+const EnvironmentalAudioExposure = new mongoose.Schema(
+  {
+    userID: {
+      type: String,
+      index: true,
+      required: true
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    hkID: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timeseries: {
+      timeField: 'startDate',
+    },
+  }
+);
+
+export default mongoose.model<IEnvironmentalAudioExposure & mongoose.Document>(
+  'EnvironmentalAudioExposure',
+  EnvironmentalAudioExposure,
+);
