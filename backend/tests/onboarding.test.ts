6511118fab63801c2ebd87a44be1cf83476d446f
matherg <gbomather@gmail.com>
2023-03-01 15:10:31 -0500

    Initial test files and interface fix for testing

---
+++ backend/tests/onboarding.test.ts
@@ -0,0 +1,30 @@
+import { IOnboardingInputDTO } from '../src/interfaces/IOnboarding';
+import OnboardingService from '../src/services/onboarding';
+import Onboarding from '../src/models/onboarding';
+import LoggerInstance from '../src/loaders/logger';
+import { EventDispatcher as EventDispatcherClass } from 'event-dispatch';
+// Next 4 lines required in every test file
+const db = require('./db');
+
+beforeAll(async () => await db.connect(), 18000);
+let mongoServer: any;
+
+afterEach(async () => await db.clearDatabase());
+
+afterAll(async () => await db.closeDatabase());
+
+const eventDispatcher = new EventDispatcherClass();
+const onboardingServiceInstance = new OnboardingService(Onboarding, LoggerInstance, eventDispatcher);
+
+describe('Add onboarding document to database', () => {
+  it('Add document', async done => {
+    const onboardingExample: IOnboardingInputDTO = {
+      userID: 'userTest',
+      nickname: 'Timmy',
+    };
+    const { onboarding } = await onboardingServiceInstance.addOnboarding(onboardingExample);
+    const onboardingDB = await Onboarding.findById(onboarding.userID);
+    expect(onboardingDB.nickname).toEqual('Timmy');
+    done();
+  });
+});
