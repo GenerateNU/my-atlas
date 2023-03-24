4318ba6b309872ce8da7ef4cbfe26d35bb8c3672
akshayd2020 <63755431+akshayd2020@users.noreply.github.com>
2023-03-24 00:16:35 -0400

    Add first component (#20)

--- frontend/src/screens/auth/AccessScreen.tsx
+++ frontend/src/screens/auth/AccessScreen.tsx
@@ -1,3 +1,5 @@
+import { Container } from 'native-base';
+import { background } from 'native-base/lib/typescript/theme/styled-system';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, Button, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -9,24 +11,26 @@ const AccessScreen = () => {
   const signIn = async () => {
     isLoading(true);
     try {
-        await auth.signIn();
+      await auth.signIn();
     } catch (error) {
-        isLoading(false);
+      isLoading(false);
     }
   };
 
   useEffect(() => {
     console.log(loading);
-  }, [loading])
+  }, [loading]);
 
   return (
-    <SafeAreaView>
-      <Text>ACCESS SCREEN</Text>
-      {loading ? (
-        <ActivityIndicator color={'#000'} animating={true} size="small" />
-      ) : (
-        <Button title="Sign In" onPress={signIn} />
-      )}
+    <SafeAreaView style={{ flex:1, backgroundColor: '#F1C3A9' }}>
+      <Container bgColor={'lighterPurple'} h={"full"} w={"full"} maxWidth="100%" maxHeight="100%">
+        <Text>ACCESS SCREEN</Text>
+        {loading ? (
+          <ActivityIndicator color={'#000'} animating={true} size="small" />
+        ) : (
+          <Button title="Sign In" onPress={signIn} />
+        )}
+      </Container>
     </SafeAreaView>
   );
 };
