3ee85d6578a1bf83c438a5c09d0d70954792fa59
kzeng24 <kathyzeng2002@gmail.com>
2023-03-30 18:30:48 -0400

    second commit

--- frontend/src/screens/app/HomeScreen.tsx
+++ frontend/src/screens/app/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ProgressBar from '../../components/ProgressBar';
 import Question from '../../components/Question';
 import { useAuth } from '../../contexts/Auth';
+import { Progress } from 'native-base';
 
 const HomeScreen = () => {
   const [loading, isLoading] = useState(false);
@@ -13,8 +14,18 @@ const HomeScreen = () => {
     await auth.signOut();
   };
 
+  const alert = () => {
+    Alert.alert('You tapped a button!');
+  }
+
   return (
     <SafeAreaView>
+      <ProgressBar
+        progress={20}
+        hasSkip={true}
+        hasProgress={true}
+        backFunction={alert}
+        skipFunction={alert}></ProgressBar>
       <Text>HOME SCREEN</Text>
       <Button title="Sign Out" onPress={signOut} />
       {auth.authData ? (
@@ -26,7 +37,6 @@ const HomeScreen = () => {
         question={
           'Have you sought behavioral health or wellness care in the past?'
         }></Question>
-        <ProgressBar progress={20} hasSkip={true} ></ProgressBar>
     </SafeAreaView>
   );
 };
