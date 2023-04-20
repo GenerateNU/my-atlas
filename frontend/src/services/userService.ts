f564d4247b5c0b92e2ef1d49bddfc94f6def2a10
Dave Budhram <davebudhram18@gmail.com>
2023-04-20 11:53:14 -0400

    should only add data to backend if the last retrieval date was before today

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -22,3 +22,25 @@ export const getUser = (token: string): Promise<IUser> => {
       });
     });
   };
+
+  export const updateUserDataDate = (_id: string, date: Date, token: string): Promise<IUser> => {
+    const headers = {
+        'Authorization': 'Bearer ' + token,
+    }
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        axios
+          .patch('http://localhost:3000/api/users/updateUserDataDate', {_id: _id, date: date},{headers})
+          .then(
+            response => {
+              console.log(response.data);
+              resolve(response.data);
+            },
+            error => {
+              console.log(error.response.data.errors.message);
+              reject(error.response.data.errors.message);
+            },
+          );
+      });
+    });
+  };
